589f518ce643d71d8e4b81b9accf12fadb928bc9
M Somerville <matthew-github@dracos.co.uk>
2020-07-15 13:14:57 +0100

    Version 1.1.

--- README.txt
+++ README.txt
@@ -17,3 +17,4 @@ Contributors
 - Matthew Somerville
 - Louise Crow
 - Andy Gimblett
+- ShaheedHaque
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='uk-postcode-utils',
-    version='1.0',
+    version='1.1',
     author='Matthew Somerville',
     author_email='matthew@mysociety.org',
     packages=['ukpostcodeutils', 'ukpostcodeutils.test'],
